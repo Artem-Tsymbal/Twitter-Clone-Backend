4d4ddecced511ffb84f06c90976a528d249d219f
Artem <artem@MacBook-Pro-Artem.local>
2021-02-04 20:12:27 +0200

    Feature/added_sign-up

---
+++ server.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import './core/db';
+
+import express from 'express';
+import { UserCtrl } from './controllers/UserController';
+import { registerValidations } from './validations/validator';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', UserCtrl.index);
+app.post('/users', registerValidations, UserCtrl.create);
+app.get('/users/verify', registerValidations, UserCtrl.verify);
+
+app.listen(process.env.PORT, (): void => {
+  console.log('Server started')
+});
